3c33e0bf376e3dc3320b4d1416d2685f5ad05dbe
Vidhi Nagar <vidhinagr6644@gmail.com>
2026-03-04 16:01:40 +0530

    add frontend url in server side files

--- contollers/documentController.js
+++ contollers/documentController.js
@@ -101,7 +101,8 @@ export const sendSignatureRequest = async (req, res) => {
     });
 
     // DYNAMIC LINK: Ye automatic backend environment se URL uthayega
-    const frontendBaseUrl = "https://pdf-sign-app-frontend.vercel.app";
+    const frontendBaseUrl =
+      "https://doc-sign-frontend-f2dcstkx2-vidhi-nagars-projects-f2385878.vercel.app";
     const signLink = `${frontendBaseUrl}/sign-external/${token}`;
 
     await sendEmail(recipientEmail, "Signature Required", signLink);
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pdf-sign-app-frontend.vercel.app",
+    "https://doc-sign-frontend-f2dcstkx2-vidhi-nagars-projects-f2385878.vercel.app",
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");

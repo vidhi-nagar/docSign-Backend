5d60c65d1313fbee822d07e037ba43e113714639
Vidhi Nagar <vidhinagr6644@gmail.com>
2026-03-05 21:58:41 +0530

    Fixed final path regex error by removing wildcard

--- server.js
+++ server.js
@@ -57,7 +57,20 @@ app.use(
   }),
 );
 
-app.options(cors());
+// app.options(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 //API Endpoints
 app.get("/", (req, res) => {

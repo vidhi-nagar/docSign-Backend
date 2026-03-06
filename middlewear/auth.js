41fdfc664a36c71179cbbf44fb5c812218b8ef61
Vidhi Nagar <vidhinagr6644@gmail.com>
2026-03-06 22:45:08 +0530

    Fix: Express wildcard path error

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const userAuth = async (req, res, next) => {
-  const { token } = req.cookies;
-
+  // const { token } = req.cookies;
+  const authHeader = req.headers.authorization;
+  const token = req.cookies.token || (authHeader && authHeader.split(" ")[1]);
   if (!token) {
     return res.status(400).json({ message: "No token,authorization denied" });
   }
@@ -12,7 +13,7 @@ export const userAuth = async (req, res, next) => {
 
     console.log("user ki details auth me", decodedToken);
     if (decodedToken.id) {
-      req.user = { id: decodedToken.id };
+      req.user = decodedToken;
       console.log("user ki details auth me", req.user);
       next();
     } else {
